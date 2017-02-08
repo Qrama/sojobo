ce47aff91c3cc09ecb4badd1f1a8a5e2b134cf6a
Mathijs Moerman <mathijs.moerman@qrama.io>
2017-02-08 15:58:47 +0100

    Correct year

--- requires.py
+++ requires.py
@@ -1,5 +1,5 @@
 # !/usr/bin/env python3
-# Copyright (C) 2016  Qrama
+# Copyright (C) 2017  Qrama
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
